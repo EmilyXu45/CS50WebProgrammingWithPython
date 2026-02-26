a286ca7de4d5aa725a7d15af6944b46032e3a190
Emily Xu <emilyxu030@gmail.com>
2026-02-27 00:03:34 +0800

    Data structures

--- Sequences.py
+++ Sequences.py
@@ -1,5 +1,5 @@
 coordinate = (10.0, 20.0)
-# list: sequence of mutable values
-# tuple: sequence of immutable values
-# set: Collection of unique values
-#dict: Collection of key-value pairs
+# list: sequence of mutable values (can change the elements in the list)
+# tuple: sequence of immutable values (values that can't be changed in the tuple)
+# set: Collection of unique values (No order; every value appear exactly once)
+#dict: Collection of key-value pairs 
