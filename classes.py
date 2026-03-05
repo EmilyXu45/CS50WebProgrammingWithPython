3b2f4bb1b8ff6d98b90f73c5f06eae12b205b4ac
Emily Xu <emilyxu030@gmail.com>
2026-03-05 23:55:44 +0800

    Class points

---
+++ classes.py
@@ -0,0 +1,7 @@
+#Classes are templates for a type of object
+class Point():
+    def __init__(self, x, y):
+    # Magic method: self references the object that we are dealing with
+         self.x = x
+         self.y = y
+         
