ef2678aa74e2e8086aec78bbde9a5585755d950a
Emily Xu <emilyxu030@gmail.com>
2026-03-08 00:01:37 +0800

    using method to check for open seats

--- flight.py
+++ flight.py
@@ -3,7 +3,10 @@ class Flight():
         self.capacity = capacity
         self.passengers = []
     def add_passenger(self, name):
+        if not self.open_seat():
+            return False
         self.passengers.append(name)
+        return True
     def open_seats(self):
         return self.capacity - len(self.passengers)
 
