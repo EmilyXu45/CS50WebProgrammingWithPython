6e569722e4bfb5869a7f8f469edd9b3e64fc99ee
Emily Xu <emilyxu030@gmail.com>
2026-03-07 23:59:06 +0800

    Added a passenger method

--- flight.py
+++ flight.py
@@ -2,5 +2,7 @@ class Flight():
     def __init__(self, capacity):
         self.capacity = capacity
         self.passengers = []
+    def add_passenger(self, name):
+        self.passengers.append(name)
 
 flight = Flight(3)
