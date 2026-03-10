d8858ad966d459a4c5d7057d7a37b984f5e7f0d6
Emily Xu <emilyxu030@gmail.com>
2026-03-11 00:18:06 +0800

    Condensing the key and using lambda functions

--- lambda.py
+++ lambda.py
@@ -4,7 +4,7 @@ people = [
     {"name": "Draco", "house": "Slytherin"}]
 
 def f(person):
-    return person["name"]
+    return person["house"]
 people.sort(key=f)
 #Key function lets the program know that it is sorting the dictionary using the function f (sorting by name)
 print(people)
