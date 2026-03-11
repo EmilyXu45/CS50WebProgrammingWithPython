50615b79b818c34ad5dfce5debf043724375fee5
Emily Xu <emilyxu030@gmail.com>
2026-03-11 23:17:00 +0800

    Added a process that can be used to test for errors later

---
+++ exceptions.py
@@ -0,0 +1,5 @@
+x = int(input("X: "))
+y = int(input("Y: "))
+
+result = x/y
+print(f"{x}/{y}={result}")
--- lambda.py
+++ lambda.py
@@ -3,8 +3,10 @@ people = [
     {"name": "Cho", "house": "Ravenclaw"},
     {"name": "Draco", "house": "Slytherin"}]
 
-def f(person):
-    return person["house"]
-people.sort(key=f)
+#def f(person):
+#   return person["house"]
+#people.sort(key=f)
 #Key function lets the program know that it is sorting the dictionary using the function f (sorting by name)
+
+people.sort(key=lambda person: person["name"])
 print(people)
