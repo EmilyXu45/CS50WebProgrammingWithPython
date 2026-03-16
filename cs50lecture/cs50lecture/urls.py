ba12f7412ff508a478a917adf54b6d98eaa88397
Emily Xu <emilyxu030@gmail.com>
2026-03-16 23:02:59 +0800

    Imported urls from hello/urls.py into main urls.py

--- cs50lecture/cs50lecture/urls.py
+++ cs50lecture/cs50lecture/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', include("hello.urls"))
+    # importing all the urls from hello/urls.py and including them in the main urls.py
 
 ]
