d956584e5af65e8cb5fbb590144e2b5e746e6b38
Emily Xu <emilyxu030@gmail.com>
2026-03-16 23:18:59 +0800

    Adding a specific URL to a main application

--- cs50lecture/hello/views.py
+++ cs50lecture/hello/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def Emily(request):
+    return HttpResponse("Hello, Emily!")
